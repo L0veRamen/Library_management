d50305c87304c327e1a251145eb235af602386d5
JC <jasonqc9103@gmail.com>
2022-11-24 21:41:18 -0500

    add form validation

--- vue/src/views/user/AddUser.vue
+++ vue/src/views/user/AddUser.vue
@@ -1,11 +1,11 @@
 <template>
   <div style="width: 80%">
     <div style="margin-bottom: 30px">Add User</div>
-    <el-form :inline="true" :model="form" label-width="100px" >
-      <el-form-item label="Name">
+    <el-form :inline="true" :model="form" :rules="rules" ref="ruleForm" label-width="100px" >
+      <el-form-item label="Name" prop="name">
         <el-input v-model="form.name" placeholder="please enter name"></el-input>
       </el-form-item>
-      <el-form-item label="Age">
+      <el-form-item label="Age" prop="age" >
         <el-input v-model="form.age" placeholder="please enter age"></el-input>
       </el-form-item>
       <el-form-item label="Sex">
@@ -38,24 +38,54 @@ import request from "@/utils/request"
 export default {
   name: "AddUser",
   data(){
+    const checkAge = (rule, value, callback) => {
+      if (!value) {
+        return callback(new Error('age is null'));
+      }
+      if (!/^[0-9]+$/.test(value)) {
+        callback(new Error('please enter number'));
+      }
+      if (parseInt(value)>120 || parseInt(value)<=0){
+        callback(new Error('please valid number'));
+      }
+      callback()
+    }
     return{
       form:{
         sex: 'M'
+      },
+      rules: {
+        name: [
+          {required: true, message: 'please enter name', trigger: 'blur'},
+        ],
+        age:[
+          {validator: checkAge,trigger:'blur'}
+        ]
       }
-    }
-  },
+      }
+    },
   methods:{
     save(){
-      request.post('/user/save', this.form).then(res =>{
-        console.log("form data: "+this.form)
-        if (res.code === '200'){
-          this.$notify.success('Add User Successfully !!!')
-          this.form = {sex: 'M'}
-          this.$router.push("/user")
-        }else{
-          this.$notify.error(res.msg)
+      this.$refs['ruleForm'].validate((valid) => {
+        if (valid) {
+          request.post('/user/save', this.form).then(res =>{
+            console.log("form data: "+this.form)
+            if (res.code === '200'){
+              this.$notify.success('Add User Successfully !!!')
+              // this.form = {sex: 'M'}
+              this.$refs['ruleForm'].resetFields()
+              this.$router.push("/user")
+            }else{
+              this.$notify.error(res.msg)
+            }
+          })
+        } else {
+          console.log('error submit!!');
+          return false;
         }
-      })
+      });
+
+
     }
   }
 }
